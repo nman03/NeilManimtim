f1dc5aaef0c120f927cf672993f7f71ebebe9867
Neil <nm31997@gmail.com>
2019-10-23 12:24:07 -0700

    testing BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Projects } from './components/Projects';
 
